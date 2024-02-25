1435a16ec42cb6294ece710d1a042652c9bb662a
Shubham Sharma <shbh541@gmail.com>
2024-02-25 22:29:41 +0530

    Fixed the remove water functionality

--- src/Bucket.jsx
+++ src/Bucket.jsx
@@ -6,8 +6,12 @@ export default function Bucket({ val, buffer }) {
       <div title={buffer.toFixed(2)}>{val.toFixed(2)}</div>
       <div className="bucket">
         <div
-          style={{ height: `${val / 10}%`, width: "100%" }}
-          className="bucket_animation"
+          style={{
+            height: `${val / 10}%`,
+            width: "100%",
+            backgroundColor: "green",
+          }}
+          className={val.toFixed(2) === "0.00" ? "" : "bucket_animation"}
         ></div>
       </div>
     </>
